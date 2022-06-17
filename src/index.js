e93cc673f2fe5d0613e044ef21d7ba077b320aaf
Hansel Warrior <jaheelwarr@gmail.com>
2022-06-17 16:31:10 -0600

    added redux

--- src/index.js
+++ src/index.js
@@ -2,6 +2,38 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { createStore } from 'redux';
+
+//Action
+const increment = () => {
+  return {
+    type: 'INCREMENT'
+  }
+}
+
+const decrement = () => {
+  return {
+    type: 'DECREMENT'
+  }
+}
+
+//Reducer
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+  }
+}
+
+let store = createStore(counter);
+
+//Display in console
+store.subscribe(() => console.log(store.getState()));
+
+//Dispatch
+store.dispatch(increment());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
