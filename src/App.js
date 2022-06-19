499f74ef739825272a734b4b44e734ba3b731d2f
Hansel Warrior <jaheelwarr@gmail.com>
2022-06-18 19:06:30 -0600

    added global styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import NavBar from "./components/NavBar";
 import Home from "./components/Home";
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
       <Home />
     </div>
     
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import NavBar from "/home/jaheel/Shopping-cart/src/components/NavBar.js";
 
 function Home() {
 
@@ -7,14 +8,18 @@ function Home() {
     display: flex;
     flex-direction: column;
     align-items: center;
-    justify-content: center
+    justify-content: center;
+    margin-top: 30vh;
   `
 
   return (
-    <div className='d-flex flex-column align-items-center justify-content-center'>
-      <h2 className='font-weight-bold'>Come for the phone,<br></br>stay for the price.</h2>
-      <a href="#" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Shop Now!</a>
-    </div>
+    <section className='bg-dark'>
+      <NavBar />
+      <HomeContainer>
+        <h2 className='font-weight-bold'>Come for the phone,<br></br>stay for the price.</h2>
+        <a href="#" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Shop Now!</a>
+      </HomeContainer>
+    </section>
   )
 }
 
